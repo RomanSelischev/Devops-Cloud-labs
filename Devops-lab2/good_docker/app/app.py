b08ffe26583a9bfc617ecd515e02818c7f9d4d13
Roman Selischev <9242801@mail.ru>
2025-08-31 05:17:20 +0300

    add devops lab2

---
+++ Devops-lab2/good_docker/app/app.py
@@ -0,0 +1,14 @@
+from flask import Flask, jsonify
+import os
+from dotenv import load_dotenv
+
+load_dotenv(dotenv_path="/good_docker/.env")
+app = Flask(__name__)
+
+@app.route('/')
+def HelloWorld():
+    secret_token = os.getenv("SECRET_TOKEN", "Токен не найден")
+    return jsonify({"message": "Hello, DevOps World!", "token": secret_token})
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
